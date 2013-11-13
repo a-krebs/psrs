1d0731449fddbd76c31ef6481edd588239709c0e
Aaron Krebs <akrebs@ualberta.ca>
2013-11-12 21:22:10 -0700

    Timing variables and CSV output.

--- main.c
+++ main.c
@@ -14,17 +14,32 @@
 
 int main(int argc, char **argv)
 {
+	/* local variables */
 	int size = -1;
 	int rank = -1;
-	double tStart = 0;
-	double tEnd = 0;
 	char hname[ 256 ];
 	struct arguments args;
+	/* locals for timing */
+	double tStart = 0;
+	double tPhase1S = 0;	/* start of phase 1 */
+	double tPhase1E = 0;	/* end of phase 1 */
+	double tPhase2S = 0;	/* start of phase 2 */
+	double tPhase2E = 0;	/* end of phase 2 */
+	double tPhase3S = 0;	/* start of phase 3 */
+	double tPhase3E = 0;	/* end of phase 3 */
+	double tPhase4S = 0;	/* start of phase 4 */
+	double tPhase4E = 0;	/* end of phase 4 */
+	/* "phase" 5 is the sorted-list-concatenation phase */
+	double tPhase5S = 0;	/* start of phase 5 */
+	double tPhase5E = 0;	/* end of phase 5 */
+	double tEnd = 0;
 
+	/* initialize MPI and get size and rank */
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+	/* parse command line arguments */
 	memset((void*)&args, 0, sizeof(struct arguments));
 	if (parse_args(argc, argv, &args, size) != 0) {
 		MPI_Finalize();
@@ -43,13 +58,22 @@ int main(int argc, char **argv)
 	tEnd = MPI_Wtime();
 
 	/*
-	 * print
-	 * rank, size, host, nElem, seed, time
+	 * CSV output
+	 * rank, size, host, nElem, seed,
+	 * phase 1 time, phase 2 time, phase 3 time, phase 4 time,
+	 * phase 5 (sorted list concatenation phase) time, total time
 	 */
-	printf("%d, %d, %s, %d, %d, %f\n",
-	    rank, size, hname, args.nElem, args.seed, tEnd- tStart);
+	printf("%d, %d, %s, %d, %d, %f, %f, %f, %f, %f, %f\n",
+	    rank, size, hname, args.nElem, args.seed,
+	    tPhase1E - tPhase1S,
+	    tPhase2E - tPhase2S,
+	    tPhase3E - tPhase3S,
+	    tPhase4E - tPhase4S,
+	    tPhase5E - tPhase5S,
+	    tEnd - tStart);
 
+
+	/* clean up and exit */
 	MPI_Finalize();
-
 	exit(EXIT_SUCCESS);
 }
