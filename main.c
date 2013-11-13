f9b8df952fe21b19ac254eedbf7f9893aec027b3
Aaron Krebs <akrebs@ualberta.ca>
2013-11-12 20:17:23 -0700

    Toying with some timing things.

--- main.c
+++ main.c
@@ -3,24 +3,41 @@
  * Aaron Krebs <akrebs@ualberta.ca>
  */
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>	/* For gethostname() */
 #include <string.h>	/* For memset() */
 #include "mpi.h"
 
+#include "macros.h"
+
 int main(int argc, char **argv)
 {
-	int size, rank;
+	int size = -1;
+	int rank = -1;
+	double tStart = 0;
+	double tEnd = 0;
 	char hname[ 256 ];
 
-	MPI_Init( &argc, &argv );
-	MPI_Comm_size( MPI_COMM_WORLD, &size );
-	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	memset( (void*)hname, 0, 256 );
-	gethostname( hname, 255 );
+	tStart = MPI_Wtime();
+	
+	memset((void*)hname, 0, 256);
+	gethostname(hname, 255);
 
-	printf( "%d of %d running on %s\n", rank, size, hname );
+	MASTER {
+		sleep(10);
+	}
+
+	tEnd = MPI_Wtime();
+
+	printf("%d of %d running on %s took %f\n",
+	    rank, size, hname, tEnd- tStart);
 
 	MPI_Finalize();
+
+	exit(EXIT_SUCCESS);
 }
